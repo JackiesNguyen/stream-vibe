46552bf38a138c9a0e5eb4644844dc0cd61eac64
vanutcho123 <hainguyen.061001@gmail.com>
2024-04-24 17:33:15 +0700

    feat: custom header component

---
+++ frontend/src/constants/nav.ts
@@ -0,0 +1,15 @@
+export const headerLinks = {
+  home: {
+    title: 'Home',
+    path: '/',
+  },
+  MoviesShows: {
+    title: 'Movies & Shows',
+    path: '/about',
+  },
+  Support: {
+    title: 'Support',
+    path: '/contact',
+  },
+}
+export const footerLinks = {}
